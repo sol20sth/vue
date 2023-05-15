0c96d459de9021be140e7d1d1439e9ae09ed98a5
김태형 <gimtaehyeong@gimtaehyeong-ui-MacBookAir.local>
2023-05-15 21:31:37 +0900

    0515

--- 실습8/server/todos/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class Todo(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    is_completed = models.BooleanField(default=False)
--- 실습8/server/todos/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Todo
-
-
-class TodoSerializer(serializers.ModelSerializer):
-    username  = serializers.CharField(source="user.username", read_only=True)
-    class Meta:
-        model = Todo
-        fields = "__all__"
